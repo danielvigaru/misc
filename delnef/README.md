2fa3a34e56e42d57448939aa9c5c3cfc309b3083
Daniel Vigaru <22689627+grasozauru@users.noreply.github.com>
2021-03-15 21:14:51 +0200

    first commit

---
+++ delnef/README.md
@@ -0,0 +1,3 @@
+Tool for cleaning up a folder of camera photos. After deleting the JPG run this in the respective folder to clean up the remaining files.
+
+Using `recycle` from the [CmdUtils](http://www.maddogsw.com/cmdutils/) collection.
---
+++ delnef/delnef.bat
@@ -0,0 +1,40 @@
+@echo off
+setlocal enabledelayedexpansion
+
+@REM initialising number of deleted NEFs to 0
+SET /A CNEF=0 
+@REM initialising number of deleted XMPs to 0
+SET /A CXMP=0
+
+@REM for every NEF in the current 
+@REM folder check for a corresponding 
+@REM JPG, if not found, delete NEF
+FOR %%N IN (*.NEF) DO (
+	SET PNEF=%%N
+	SET PJPG=!PNEF:NEF=JPG!
+	IF NOT EXIST "!PJPG!" ( 
+		RECYCLE /F "%%N"
+		ECHO !PNEF! DELETED
+		SET /A CNEF+=1
+	)
+)
+
+@REM for every XMP in the current f
+@REM older check for a corresponding 
+@REM NEF, if not found, delete XMP
+FOR %%X IN (*.xmp) DO (
+	SET PXMP=%%X
+	SET PNEF=!PXMP:xmp=NEF!	
+	IF NOT EXIST "!PNEF!" ( 
+		RECYCLE /F "%%X"
+		ECHO !PXMP! DELETED
+		SET /A CXMP+=1
+	)
+)
+
+ECHO .
+ECHO .
+ECHO .
+ECHO NEF FILES DELETED: !CNEF!
+ECHO XMP FILES DELETED: !CXMP!
+PAUSE
