92b335a63dcdc2293dcf6bd65b7413c076d23edc
Daniel Vigaru <22689627+danielvigaru@users.noreply.github.com>
2021-06-03 00:51:31 +0300

    reworked display logic

--- userscripts/IMDb/imdb.js
+++ userscripts/IMDb/imdb.js
@@ -3,7 +3,7 @@
 // @description      Links to torrents and trakt directly from imdb page
 // @license          MIT
 // @include          https://www.imdb.com/*
-// @version          1.1.3
+// @version          2.0.0
 // @updateURL        https://github.com/danielvigaru/misc/raw/main/userscripts/IMDb/imdb.js
 // @grant            none
 // ==/UserScript==
@@ -18,72 +18,20 @@ let movieId = getIMDBid();
 
 if (movieId) {
 
-  // RARBG
-  let rarbg = document.createElement('div');
-  rarbg.innerHTML = '<a id="rarbgSearchButton">RARBG</a>';
-  rarbg.style.display = "inline-block";
-  rarbg.style.position = "absolute";
-  rarbg.style.left = "1em";
-  rarbg.style.top = "4.5em";
-  rarbg.style.zIndex = '9999';
+  let linkConstructor = "<a style='text-decoration:none; color:black;' target='_blank'"
+  let linkRarbg = `${linkConstructor} href='https://rarbgmirror.org/torrents.php?imdb=tt${movieId}'">RARBG</a>`
+  let linkFilelist = `${linkConstructor} href='https://filelist.io/browse.php?search=tt${movieId}'">FileList</a>`
+  let linkTrakt = `${linkConstructor} href='https://trakt.tv/search/imdb?q=tt${movieId}'">Trakt</a>`
 
-  document.body.append(rarbg);
+  let links = document.createElement('div');
+  links.innerHTML = `${linkRarbg} | ${linkFilelist} | ${linkTrakt}`
+  links.style.display = "block";
+  links.style.position = "absolute";
+  links.style.left = "1rem";
+  links.style.top = "4.6rem";
+  links.style.fontFamily = "'Roboto','Helvetica','Arial',sans-serif";
+  links.style.zIndex = '9999';
 
-  let icon_rarbg = document.getElementById('rarbgSearchButton');
-  icon_rarbg.style.background = '#F5C518';
-  icon_rarbg.style.color = 'black';
-  icon_rarbg.style.fontWeight = '800';
-  icon_rarbg.style.padding = '5px';
-  icon_rarbg.style.borderRadius = '7px';
-  icon_rarbg.style.textDecoration = 'none';
-  icon_rarbg.style.fontSize = '0.8em';
-  icon_rarbg.style.fontFamily = "'Roboto','Helvetica','Arial',sans-serif";
-  icon_rarbg.href = 'https://rarbgmirror.org/torrents.php?imdb=tt' + movieId;
-  icon_rarbg.target = '_blank';
+  document.body.appendChild(links);
 
-  // filelist
-  let filelist = document.createElement('div');
-  filelist.innerHTML = '<a id="filelistSearchButton">FileList</a>';
-  filelist.style.display = "inline-block";
-  filelist.style.position = "absolute";
-  filelist.style.left = "5em";
-  filelist.style.top = "4.5em";
-  filelist.style.zIndex = '9999';
-
-  document.body.append(filelist);
-
-  let icon_filelist = document.getElementById('filelistSearchButton');
-  icon_filelist.style.background = '#F5C518';
-  icon_filelist.style.color = 'black';
-  icon_filelist.style.fontWeight = '800';
-  icon_filelist.style.padding = '5px';
-  icon_filelist.style.borderRadius = '7px';
-  icon_filelist.style.textDecoration = 'none';
-  icon_filelist.style.fontSize = '0.8em';
-  icon_filelist.style.fontFamily = "'Roboto','Helvetica','Arial',sans-serif";
-  icon_filelist.href = 'https://filelist.io/browse.php?search=tt' + movieId;
-  icon_filelist.target = '_blank';
-
-  // trakt
-  let trakt = document.createElement('div');
-  trakt.innerHTML = '<a id="traktSearchButton">Trakt</a>';
-  trakt.style.display = "inline-block";
-  trakt.style.position = "absolute";
-  trakt.style.left = "9em";
-  trakt.style.top = "4.5em";
-  trakt.style.zIndex = '9999';
-
-  document.body.append(trakt);
-
-  let icon_trakt = document.getElementById('traktSearchButton');
-  icon_trakt.style.background = '#F5C518';
-  icon_trakt.style.color = 'black';
-  icon_trakt.style.fontWeight = '800';
-  icon_trakt.style.padding = '5px';
-  icon_trakt.style.borderRadius = '7px';
-  icon_trakt.style.textDecoration = 'none';
-  icon_trakt.style.fontSize = '0.8em';
-  icon_trakt.style.fontFamily = "'Roboto','Helvetica','Arial',sans-serif";
-  icon_trakt.href = 'https://trakt.tv/search/imdb?q=tt' + movieId;
-  icon_trakt.target = '_blank';
 }
