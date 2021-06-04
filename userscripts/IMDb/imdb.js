d05a84c3b290310527a426db3a0f44693006b2e1
Daniel Vigaru <22689627+danielvigaru@users.noreply.github.com>
2021-06-05 02:18:30 +0300

    execute script onload

--- userscripts/IMDb/imdb.js
+++ userscripts/IMDb/imdb.js
@@ -3,7 +3,7 @@
 // @description      Links to torrents and trakt directly from imdb page
 // @license          MIT
 // @include          https://www.imdb.com/*
-// @version          2.1.0
+// @version          2.2.0
 // @updateURL        https://github.com/danielvigaru/misc/raw/main/userscripts/IMDb/imdb.js
 // @grant            none
 // ==/UserScript==
@@ -14,22 +14,25 @@ function getIMDBid() {
   return id[1];
 }
 
-let movieId = getIMDBid();
+window.onload = () => {
 
-if (movieId) {
+  let movieId = getIMDBid();
 
-  let linkConstructor = "<a style='text-decoration:none; color:black;' target='_blank'"
-  let linkRarbg = `${linkConstructor} href='https://rarbgmirror.org/torrents.php?imdb=tt${movieId}'">RARBG</a>`
-  let linkFilelist = `${linkConstructor} href='https://filelist.io/browse.php?search=tt${movieId}'">FileList</a>`
-  let linkTrakt = `${linkConstructor} href='https://trakt.tv/search/imdb?q=tt${movieId}'">Trakt</a>`
+  if (movieId) {
 
-  let links = document.createElement('div');
-  links.innerHTML = `${linkTrakt} | ${linkFilelist} | ${linkRarbg}`
-  links.style.fontFamily = "'Roboto','Helvetica','Arial',sans-serif";
-  links.style.padding = '0.5em 1em';
+    let linkConstructor = "<a style='text-decoration:none; color:black;' target='_blank'"
+    let linkRarbg = `${linkConstructor} href='https://rarbgmirror.org/torrents.php?imdb=tt${movieId}'">RARBG</a>`
+    let linkFilelist = `${linkConstructor} href='https://filelist.io/browse.php?search=tt${movieId}'">FileList</a>`
+    let linkTrakt = `${linkConstructor} href='https://trakt.tv/search/imdb?q=tt${movieId}'">Trakt</a>`
 
-  let banner = document.querySelector('.banner');
-  banner.innerHTML = ''; // clear the content of the banner
-  banner.appendChild(links);
+    let links = document.createElement('div');
+    links.innerHTML = `${linkTrakt} | ${linkFilelist} | ${linkRarbg}`
+    links.style.fontFamily = "'Roboto','Helvetica','Arial',sans-serif";
+    links.style.padding = '0.5em 1em';
 
+    let banner = document.querySelector('.banner');
+    banner.innerHTML = ''; // clear the content of the banner
+    banner.appendChild(links);
+
+  }
 }
