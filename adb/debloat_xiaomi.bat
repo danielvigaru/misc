a2907784b99585e5fa9dde39f7259eaf4b359333
Daniel Vigaru <22689627+danielvigaru@users.noreply.github.com>
2021-06-15 19:05:29 +0300

    init debloat xiaomi script

---
+++ adb/debloat_xiaomi.bat
@@ -0,0 +1,31 @@
+@echo off
+
+@REM This is the way I set the script, you can comment   
+@REM or uncomment any line to match your preferences.
+@REM App names are in the comments.
+
+@REM How to re-enable an app:
+@REM adb shell pm enable --user 0 com.xiaomi.calendar
+
+@REM adb shell pm disable-user --user 0 com.android.thememanager @REM Themes - can't set wallpaper if disabled
+adb shell pm disable-user --user 0 com.android.mms
+adb shell pm disable-user --user 0 com.facebook.appmanager
+adb shell pm disable-user --user 0 com.facebook.services
+adb shell pm disable-user --user 0 com.facebook.system
+adb shell pm disable-user --user 0 com.google.android.youtube @REM YouTube
+adb shell pm disable-user --user 0 com.mi.android.globalminusscreen @REM App vault
+adb shell pm disable-user --user 0 com.mi.globalbrowser @REM Mi Browser
+adb shell pm disable-user --user 0 com.mipay.wallet.id
+adb shell pm disable-user --user 0 com.miui.analytics
+adb shell pm disable-user --user 0 com.miui.bugreport
+adb shell pm disable-user --user 0 com.miui.hybrid
+adb shell pm disable-user --user 0 com.miui.hybrid.accessory
+adb shell pm disable-user --user 0 com.miui.miservice @REM Services and Feedback
+adb shell pm disable-user --user 0 com.miui.player @REM Music
+adb shell pm disable-user --user 0 com.miui.weather2 @REM Weatehr
+adb shell pm disable-user --user 0 com.miui.yellowpage
+adb shell pm disable-user --user 0 com.xiaomi.calendar @REM Calendar
+adb shell pm disable-user --user 0 com.xiaomi.joyose
+adb shell pm disable-user --user 0 com.xiaomi.midrop @REM ShareMe
+
+pause
